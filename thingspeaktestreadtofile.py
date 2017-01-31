4ac740de85857b5cb78fc87348a5742e13b506f8
akshaim <akshai.kannan@gmail.com>
2017-01-31 18:15:48 +0530

    Data to file

--- thingspeaktestreadtofile.py
+++ thingspeaktestreadtofile.py
@@ -1,4 +1,7 @@
 import urllib
 response = urllib.urlopen('http://api.thingspeak.com/channels/your_channel_no/field/1/last.txt')
-data = response.read()
+data=response.read()
 print data
+file = open("thingspeak.txt","w")
+file.write(data)
+file.close()
